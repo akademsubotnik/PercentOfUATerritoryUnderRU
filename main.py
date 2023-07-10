6f7ed0b6b20a37a10ad1ff4fbfaa70b6ad174d63
Greg <44036625+akademsubotnik@users.noreply.github.com>
2023-07-10 17:34:29 +0000

    update

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("hello")
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://deepstatemap.live"
+response = requests.get(url)
+
+if response.status_code == 200:
+    soup = BeautifulSoup(response.content, 'html.parser')
+    #print(soup.parserClass)
+    #for element in soup.find_all('a'):
+    #    print(element.text)
+    #print(soup)
+    print(soup.find('div', id='map'))
+else:
+    print("Error: Could not retrieve webpage")
+
