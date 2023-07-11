f2c4ca16d78b7e5580fb137ed7d11a3ff0860928
Greg <44036625+akademsubotnik@users.noreply.github.com>
2023-07-11 03:14:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import os
 from selenium import webdriver
+import time
 
+#config
 os.environ['PATH'] += r"C:\Users\Chuwi\PycharmProjects\UaRu\chromedriver_win32\chromedriver"
 driver = webdriver.Chrome()
+driver.get("https://deepstatemap.live/")
+
+#popups
+time.sleep(5) #sleep for 1 sec
+my_element = driver.find_element("xpath", "/html/body/div[12]/div/div[1]")
+my_element.click()
+time.sleep(5) #sleep for 1 sec
+
