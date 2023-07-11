1a803bf66e0ecb20177f8cf5f60023b11ba202bc
Greg <44036625+akademsubotnik@users.noreply.github.com>
2023-07-11 18:06:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 from selenium import webdriver
-import time
 
 #config
 os.environ['PATH'] += r"C:\Users\Chuwi\PycharmProjects\UaRu\chromedriver_win32\chromedriver"
@@ -8,11 +7,25 @@ driver = webdriver.Chrome()
 driver.get("https://deepstatemap.live/")
 
 #popups
-time.sleep(5) #sleep for 1 sec
-my_element = driver.find_element("xpath", "/html/body/div[12]/div/div[1]")
-my_element.click()
-time.sleep(5) #sleep for 1 sec
-my_element2 = driver.find_element("xpath", "/html/body/div[1]/div/div[2]/button[2]")
-my_element2.click()
-time.sleep(5) #sleep for 1 sec
+try:
+    driver.implicitly_wait(3) #wait until element loads (timeout 3 seconds)
+    ad1 = driver.find_element("xpath", "/html/body/div[12]/div/div[1]")
+    ad1.click()
+    driver.implicitly_wait(3) #wait until element loads (timeout 3 seconds)
+    rules = driver.find_element("xpath", "/html/body/div[1]/div/div[2]/button[2]")
+    rules.click()
+    driver.implicitly_wait(3) #wait until element loads (timeout 3 seconds)
+except:
+    print('No element found.  Skipping')
 
+
+#map
+zapor = driver.find_element("xpath", '//*[@id="map"]/div[1]/div[6]/div/div[1]/div/i')
+#zapor.click()
+print(zapor.text)
+driver.implicitly_wait(3) #wait until element loads (timeout 3 seconds)
+
+
+#ot1 = driver.find_element("xpath", '//*[@id="map"]/div[1]/div[6]/div/div[1]/div/i')
+#print(ot1.text)
+#driver.implicitly_wait(3) #wait until element loads (timeout 3 seconds)
