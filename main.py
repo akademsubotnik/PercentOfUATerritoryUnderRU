7f9ec3bdd93179f70608c00a8cc2342fbe16104e
Greg <44036625+akademsubotnik@users.noreply.github.com>
2023-07-12 01:56:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,36 @@
 import os
+import time
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-#config
-os.environ['PATH'] += r"C:\Users\Chuwi\PycharmProjects\UaRu\chromedriver_win32\chromedriver"
-driver = webdriver.Chrome()
-driver.get("https://deepstatemap.live/")
+def setup():
+    os.environ['PATH'] += r"C:\Users\Chuwi\PycharmProjects\UaRu\chromedriver_win32\chromedriver"
+    driver = webdriver.Chrome()
+    driver.get("https://deepstatemap.live/")
+    return driver
 
-#popups
-try:
-    driver.implicitly_wait(3) #wait until element loads (timeout 3 seconds)
-    ad1 = driver.find_element("xpath", "/html/body/div[12]/div/div[1]")
-    ad1.click()
-    driver.implicitly_wait(3) #wait until element loads (timeout 3 seconds)
-    rules = driver.find_element("xpath", "/html/body/div[1]/div/div[2]/button[2]")
-    rules.click()
-    driver.implicitly_wait(3) #wait until element loads (timeout 3 seconds)
-except:
-    print('No element found.  Skipping')
+def popups(driver):
+    # popups
+    try:
+        driver.implicitly_wait(3)  # wait until element loads (timeout 3 seconds)
+        ad1 = driver.find_element("xpath", "/html/body/div[12]/div/div[1]")
+        ad1.click()
+        driver.implicitly_wait(3)  # wait until element loads (timeout 3 seconds)
+        rules = driver.find_element("xpath", "/html/body/div[1]/div/div[2]/button[2]")
+        rules.click()
+        driver.implicitly_wait(3)  # wait until element loads (timeout 3 seconds)
+    except:
+        print('No element found.  Skipping')
+    return driver
 
+driver = setup()
+driver = popups(driver)
 
 #map
-zapor = driver.find_element("xpath", '//*[@id="map"]/div[1]/div[6]/div/div[1]/div/i')
-#zapor.click()
-print(zapor.text)
+ot1 = driver.find_element("xpath", '//*[@id="map"]')
+ot1.click()
+#print(zapor.text)
 driver.implicitly_wait(3) #wait until element loads (timeout 3 seconds)
+time.sleep(5) #sleep for 1 sec
 
-
-#ot1 = driver.find_element("xpath", '//*[@id="map"]/div[1]/div[6]/div/div[1]/div/i')
-#print(ot1.text)
-#driver.implicitly_wait(3) #wait until element loads (timeout 3 seconds)
+#click an area on the map by coordiantes for each OT???
