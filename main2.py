dd3ff3e17029c586f6eec068e81fc378b4f153cd
greg <gregjsmith@gmx.com>
2023-09-09 20:12:30 +0300

    add notes

--- main2.py
+++ main2.py
@@ -21,5 +21,8 @@ driver.maximize_window()
 #driver = f1.popups(driver)
 time.sleep(5)
 
+#TO GET ELEMENT LOCATION, RUN  https://stackoverflow.com/questions/26820942/browser-developer-tools-what-is-the-position-of-the-html-element
+# $0.getBoundingClientRect() with element focused and use x and y as values for get by offset function!!!
+
 zaporozye_ot = f1.mbo(driver)
 time.sleep(10)
